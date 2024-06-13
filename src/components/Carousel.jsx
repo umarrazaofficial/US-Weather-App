d0576aca77249a3a5f8c11db5756e6502f5e0660
Umar Raza <umar@webevis.com>
2024-06-13 15:11:06 +0500

    first commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Banner from "../assets/banner.jpg";
 
 const Carousel = () => {
     return (
@@ -6,23 +7,9 @@ const Carousel = () => {
             <div id="carouselExampleFade" className="carousel slide carousel-fade">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://gamestore.com.pk/wp-content/uploads/2023/01/2-1.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://gamestore.com.pk/wp-content/uploads/2023/01/6-1.jpg" className="d-block w-100" alt="..." />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://gamestore.com.pk/wp-content/uploads/2023/01/4-1.jpg" className="d-block w-100" alt="..." />
+                        <img src={Banner} className="d-block w-100" alt="..." />
                     </div>
                 </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
             </div>
         </>
     )
